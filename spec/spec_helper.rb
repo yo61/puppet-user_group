43ddb8b27b2331e2237868d961776a234132bffa
Robin Bowes <robin.bowes@yo61.com>
2015-03-25 20:29:05 +0000

    Add rspec-puppet-facts (omitted from a previos commit), and set a puppetversion fact from an environment variable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
+
+RSpec.configure do |c|
+  c.default_facts = {
+    :puppetversion => ENV['PUPPET_VERSION'] || '3.7.0'
+  }
+end
