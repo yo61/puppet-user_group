34aa3b13f9b53da4d9b7daebc077a093be43c8c8
Robin Bowes <robin.bowes@yo61.com>
2015-03-25 20:26:44 +0000

    Use rspec-puppet-facts to test on loadsa operating systems

--- spec/defines/user_group_spec.rb
+++ spec/defines/user_group_spec.rb
@@ -1,34 +1,21 @@
 require 'spec_helper'
 
-common_facts = {
-  :concat_basedir => '/tmp',
-  :memorysize => '2 GB',
-  :processorcount => '2',
-}
-
-supported_os = [
-  {
-    :kernel => 'Linux',
-    :osfamily => 'RedHat',
-    :operatingsystem => 'redhat',
-    :operatingsystemrelease => '7.0',
-  }
-]
-
 describe '::user_group', :type => :define do
-  let :default_resource_title do 'rspec_user' end
-  let :default_params do {} end
-  supported_os.each do |os|
-    let :facts do os.merge(common_facts) end
-    let :title do default_resource_title end
-    describe "::user_group define with no parameters on #{os[:operatingsystem]} #{os[:operatingsystemrelease]}" do
-      let :params do default_params end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do facts end
+      let :default_resource_title do 'rspec_user' end
+      let :default_params do {} end
+      let :title do default_resource_title end
+      describe "::user_group define with no parameters" do
+        let :params do default_params end
 
-      it { should compile.with_all_deps }
-      it { should contain_class('user_group::params') }
-      it { should contain_user_group(default_resource_title) }
-      it { should contain_user(default_resource_title) }
-      it { should contain_group(default_resource_title) }
+        it { should compile.with_all_deps }
+        it { should contain_class('user_group::params') }
+        it { should contain_user_group(default_resource_title) }
+        it { should contain_user(default_resource_title) }
+        it { should contain_group(default_resource_title) }
+      end
     end
   end
 end
