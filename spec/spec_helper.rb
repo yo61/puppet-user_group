0eb4ad8ad240b8834bb1a79e86430b312acc4bd7
Robin Bowes <robin.bowes@yo61.com>
2015-03-26 19:07:48 +0000

    Don't set a default value for puppetversion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,6 @@ include RspecPuppetFacts
 
 RSpec.configure do |c|
   c.default_facts = {
-    :puppetversion => ENV['PUPPET_VERSION'] || '3.7.4'
+    :puppetversion => ENV['PUPPET_VERSION']
   }
 end
